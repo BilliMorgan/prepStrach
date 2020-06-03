2950330bd4e430e75ca0bfa96f55359a38aca14d
Andrii Shymko <55424136+BilliMorgan@users.noreply.github.com>
2020-06-03 13:56:49 -0400

    clear air done

---
+++ cleanAir.js
@@ -0,0 +1,38 @@
+const checkAir = function (samples, threshold) {
+  let counter = 0;
+  for (let sample of samples) {
+    if (sample === "dirty") {
+      counter++;
+    }
+  }
+  let indicator = counter/10
+  if(indicator < threshold){
+    return "Clean"
+  }else{
+    return "Polluted"
+  }
+};
+
+console.log(
+  checkAir(
+    [
+      "clean",
+      "clean",
+      "dirty",
+      "clean",
+      "dirty",
+      "clean",
+      "clean",
+      "dirty",
+      "clean",
+      "dirty",
+    ],
+    0.3
+  )
+);
+
+console.log(checkAir(["dirty", "dirty", "dirty", "dirty", "clean"], 0.25));
+
+console.log(
+  checkAir(["clean", "dirty", "clean", "dirty", "clean", "dirty", "clean"], 0.9)
+);
--- smartParking.js
+++ smartParking.js
@@ -1,23 +1,21 @@
 const whereCanIPark = function (spots, vehicle) {
-  let coordinates = [];
-
   for (let y = 0; y < spots.length; y++) {
     for (let x = 0; x < spots[y].length; x++) {
-      const spot =spots[y][x]
+      const spot = spots[y][x];
 
-      if(vehicle === "regular"){
-        if(spot === "R"){
-           return [x, y] 
+      if (vehicle === "regular") {
+        if (spot === "R") {
+          return [x, y];
         }
-      } else if (vehicle === "small"){
-        if(spot === "R" || spot === "S"){
-          return [x, y]
+      } else if (vehicle === "small") {
+        if (spot === "R" || spot === "S") {
+          return [x, y];
         }
       } else if (vehicle === "motorcycle") {
-               if (spot === "R" || spot === "S" || spot === "M") {
-                 return [x, y];
-               }
-             }
+        if (spot === "R" || spot === "S" || spot === "M") {
+          return [x, y];
+        }
+      }
     }
   }
   return false;
