b1f03eca7bc9d059fe3e09f2630af4bae5412983
Andrii Shymko <55424136+BilliMorgan@users.noreply.github.com>
2020-06-09 22:15:09 -0400

    case maker is done

---
+++ caseMaker.js
@@ -0,0 +1,12 @@
+const camelCase = function (input) {
+  return input.split(" ").map(function(word, index){
+    if(index == 0){
+      return word.toLowerCase();
+    }
+    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+  }).join("");
+};
+
+console.log(camelCase("this is a string"));
+console.log(camelCase("loopy lighthouse"));
+console.log(camelCase("supercalifragalisticexpialidocious"));
