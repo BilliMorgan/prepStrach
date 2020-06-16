c6cfdea8ff490ecc3fa10b824d9600a2d5f31e11
Andrii Shymko <55424136+BilliMorgan@users.noreply.github.com>
2020-06-16 10:01:05 -0400

    change calculator

---
+++ changeCalculator.js
@@ -0,0 +1,16 @@
+const calculateChange = function (total, cash) {
+let change = cash - total;
+console.log(change)
+
+
+let changeObject = {};
+
+return changeObject;  
+
+
+
+};
+
+console.log(calculateChange(1787, 2000));
+console.log(calculateChange(2623, 4000));
+console.log(calculateChange(501, 1000));
