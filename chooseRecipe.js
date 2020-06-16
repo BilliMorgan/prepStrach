62c93f74a5acb504011944af5cbe0930b80f66e4
Andrii Shymko <55424136+BilliMorgan@users.noreply.github.com>
2020-06-16 09:22:23 -0400

    talking calendar is done

--- chooseRecipe.js
+++ chooseRecipe.js
@@ -1,26 +1,40 @@
 const chooseRecipe = function (bakeryA, bakeryB, recipes) {
-  for (nameFirst of ingredientCheck(bakeryA, recipes)) {
-    for (nameSecond of ingredientCheck(bakeryB, recipes)) {
-      if (nameFirst === nameSecond) {
-        return nameSecond;
-      }
-    }
-  }
-};
-
-const ingredientCheck = function (bakery, recipes) {
-  let recipeName = [];
-  for (let i = 0; i < bakery.length; i++) {
-    for (let y = 0; y < recipes.length; y++) {
-      if (
-        bakery[i] === recipes[y].ingredients[0] ||
-        bakery[i] === recipes[y].ingredients[1]
-      ) {
-        recipeName.push(recipes[y].name);
-      }
+  let recipeName = "";
+  for (recipe of recipes) {
+    let ingredients = recipe.ingredients;
+    console.log(ingredients);
+    let ingrBakeryA =
+      bakeryA.includes(ingredients[0]) || bakeryA.includes(ingredients[1]);
+    let ingrBakeryB =
+      bakeryB.includes(ingredients[0]) || bakeryB.includes(ingredients[1]);
+    if (ingrBakeryA && ingrBakeryB) {
+      recipeName = recipe.name;
     }
   }
   return recipeName;
+
+  //   for (nameFirst of ingredientCheck(bakeryA, recipes)) {
+  //     for (nameSecond of ingredientCheck(bakeryB, recipes)) {
+  //       if (nameFirst === nameSecond) {
+  //         return nameSecond;
+  //       }
+  //     }
+  //   }
+  // };
+
+  // const ingredientCheck = function (bakery, recipes) {
+  //   let recipeName = [];
+  //   for (let i = 0; i < bakery.length; i++) {
+  //     for (let y = 0; y < recipes.length; y++) {
+  //       if (
+  //         bakery[i] === recipes[y].ingredients[0] ||
+  //         bakery[i] === recipes[y].ingredients[1]
+  //       ) {
+  //         recipeName.push(recipes[y].name);
+  //       }
+  //     }
+  //   }
+  //   return recipeName;
 };
 
 let bakeryA = ["saffron", "eggs", "tomato paste", "coconut", "custard"];
--- talkingCalendar.js
+++ talkingCalendar.js
@@ -1,17 +1,35 @@
 const talkingCalendar = function (date) {
-  let newDate = "";
-  let month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  const getMonth =  function(month, arr) {
-    console.log(arr)
-    console.log(month)
-    let index = arr[1] - 1;
-    console.log(index)
 
-  }
   let arr = date.split("/");
-  console.log(arr);
+  let month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+  const getMonth =  function(arr) {
+    let index = arr[1]-1;
+    return month[index];
+  };
+
+  const getDate = function(){
+  let convertNumber = Number(arr[2])
+   if(arr[2] == 1){
+     return ` ${convertNumber}st, `;
+   }
+   if(convertNumber === 2){
+     return ` ${convertNumber}nd, `;
+   }
+   if(convertNumber === 3){
+     return ` ${convertNumber}rd, `;
+   }
+   if(convertNumber >= 4){
+     return ` ${convertNumber}th, `;
+   }
+  }
+
+  const getYear = function(){
+    return arr[0]
+  }
+  return getMonth(arr) + getDate() + getYear(); 
 };
 
 console.log(talkingCalendar("2017/12/02"));
-console.log(talkingCalendar("2007/11/11"));
+console.log(talkingCalendar("2007/11/04"));
 console.log(talkingCalendar("1987/08/24"));
