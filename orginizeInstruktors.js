436b56ce9748ccc46248468fbf89439961e84daa
Andrii Shymko <55424136+BilliMorgan@users.noreply.github.com>
2020-06-17 11:34:01 -0400

    orginize Instructors

---
+++ orginizeInstruktors.js
@@ -0,0 +1,22 @@
+const organizeInstructors = function (instructors) {
+
+
+  
+};
+
+console.log(
+  organizeInstructors([
+    { name: "Samuel", course: "iOS" },
+    { name: "Victoria", course: "Web" },
+    { name: "Karim", course: "Web" },
+    { name: "Donald", course: "Web" },
+  ])
+);
+console.log(
+  organizeInstructors([
+    { name: "Brendan", course: "Blockchain" },
+    { name: "David", course: "Web" },
+    { name: "Martha", course: "iOS" },
+    { name: "Carlos", course: "Web" },
+  ])
+);
