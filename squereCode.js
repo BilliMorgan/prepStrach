67b8c513a6e4846b981ab7b3b4ffb72e36decb52
Andrii Shymko <55424136+BilliMorgan@users.noreply.github.com>
2020-07-14 09:32:36 -0400

    some logic done

--- squereCode.js
+++ squereCode.js
@@ -1,12 +1,34 @@
 const squareCode = function (message) {
-  // Put your solution here
+  let squareRoot = Math.ceil(Math.sqrt(message.split(" ").join("").length));
+  let splitMessage = message.split(" ").join("");
+  let counter = 0;
+  let newArray = [];
+  let generalArray = [];
+
+  //console.log(squareRoot)
+  //console.log(splitMessage)
+  for (let letter of splitMessage){
+    if (counter != squareRoot){
+      newArray.push(letter)
+      counter += 1
+    }
+    else if (counter === squareRoot){
+      generalArray.push(newArray)
+      counter = 0;
+    }
+
+
+  }
+  console.log(generalArray)
+  console.log(counter)
+  return newArray.join("")
 };
 
 console.log(squareCode("chill out"));
-console.log(squareCode("feed the dog"));
-console.log(squareCode("have a nice day"));
-console.log(
-  squareCode(
-    "if man was meant to stay on the ground god would have given us roots"
-  )
-);
+//console.log(squareCode("feed the dog"));
+// console.log(squareCode("have a nice day"));
+// console.log(
+//   squareCode(
+//     "if man was meant to stay on the ground god would have given us roots"
+//   )
+// );
